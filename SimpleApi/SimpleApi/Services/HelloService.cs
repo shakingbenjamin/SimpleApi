f4124e3a518f7b388f6173cd6b38fe3c39abf766
shakingbenjamin <travesben1@gmail.com>
2019-03-02 11:25:18 +0000

    initial api set up

---
+++ SimpleApi/SimpleApi/Services/HelloService.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleApi.Services
+{
+    using ServiceStack;
+    using SimpleApi.RequestDTOs;
+    using SimpleApi.ResponseDTOs;
+
+    public class HelloService : Service
+    {
+        public object Any(HelloRequest request)
+        {
+            return new HelloResponse { Result = $"Hello {request.Name}" };
+        }
+    }
+}
