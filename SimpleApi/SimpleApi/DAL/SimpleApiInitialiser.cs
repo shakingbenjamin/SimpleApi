362d4fe419ec839045550d3be77c5658a4852bed
benjamin-traves <travesben1@gmail.com>
2019-03-03 19:43:49 +0000

    updated initialiser

--- SimpleApi/SimpleApi/DAL/SimpleApiInitialiser.cs
+++ SimpleApi/SimpleApi/DAL/SimpleApiInitialiser.cs
@@ -8,10 +8,20 @@
     {
         protected override void Seed(SimpleApiContext context)
         {
+            var addresses = new List<Address>
+            {
+                new Address { NameOrNumber = "", Street = "", City = "", Province = "", PostCode = ""}
+            };
+
+            addresses.ForEach(a => context.Addresses.Add(a));
+            context.SaveChanges();
+
             var contacts = new List<Contact>
             {
-            new Contact{FirstName="Holly",LastName="Heather", Title="Miss"},
+                new Contact { FirstName="Jane", LastName="Doe", Title="Miss"},
+                new Contact { FirstName="Davie", LastName="Jones", Title="Mr" }
             };
+
 
             contacts.ForEach(c => context.Contacts.Add(c));
             context.SaveChanges();
