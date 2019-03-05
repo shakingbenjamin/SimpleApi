4f93dae4e8e6ce4806cd29f66f2ad38834984949
shakingbenjamin <travesben1@gmail.com>
2019-03-05 00:24:46 +0000

    basic omrlite usage implemented - needs improvement

--- SimpleApi/SimpleApi/DAL/SimpleApiContext.cs
+++ SimpleApi/SimpleApi/DAL/SimpleApiContext.cs
@@ -8,18 +8,20 @@ using System.Web;
 
 namespace SimpleApi.DAL
 {
-    public class SimpleApiContext : DbContext
-    {
-        public SimpleApiContext() : base("simpleapidb")
-        {
-        }
+    // Entity framework context
 
-        public DbSet<Contact> Contacts { get; set; }
-        public DbSet<Address> Addresses { get; set; }
+    //public class SimpleApiContext : DbContext
+    //{
+    //    public SimpleApiContext() : base("simpleapidb")
+    //    {
+    //    }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
-    }
+    //    public DbSet<Contact> Contacts { get; set; }
+    //    public DbSet<Address> Addresses { get; set; }
+
+    //    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    //    }
+    //}
 }
